fffaf77eaada221a2c9ce67f6c9a0babb4137e2a
Dominic Ligot <dominic.ligot@cirrolytix.com>
2023-09-11 21:20:52 +0800

    simple test

--- tests/test_geoboundaries.py
+++ tests/test_geoboundaries.py
@@ -1,17 +1,10 @@
-import os
-import geopandas as gpd
-import pytest
-import os
-import json
-import requests
-import time
-from fiona.crs import from_epsg
-from time import sleep
-import warnings
-
-warnings.filterwarnings("ignore")
-
-from .src.data_extraction.admin_boundaries import fetch_geoboundaries, save_admin_regions
-
-def test_usage():
-    save_admin_regions("PHL", 2)
+def test_functions():
+    import os
+    import json
+    import requests
+    import geopandas as gpd
+    from fiona.crs import from_epsg
+    from time import sleep
+    import warnings
+    warnings.filterwarnings("ignore")
+    from .src.data_extraction.admin_boundaries import fetch_geoboundaries, save_admin_regions
