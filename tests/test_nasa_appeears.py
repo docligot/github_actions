3aa3d99894c5cf93c8c4aa6cba78455cc3dbc5c3
Dominic Ligot <dominic.ligot@cirrolytix.com>
2023-09-11 19:55:26 +0800

    Update test_nasa_appeears.py

--- tests/test_nasa_appeears.py
+++ tests/test_nasa_appeears.py
@@ -8,4 +8,5 @@ import logging
 from .src.data_extraction.nasa_appeears import fetch_nasa_appeears
 
 def test_nasa_appeears():
+    headers = get_earthdata_token()  # Initialize headers by fetching token once
     fetch_nasa_appeears(headers, "PHL", "Zamboanga Sibugay", 2, '01-01-2021', '01-10-2021')
