a83d3d6bfca6b680111ba1d8dad79bf46ede9e5d
Dominic Ligot <dominic.ligot@cirrolytix.com>
2023-09-11 21:54:40 +0800

    Update test_osm.py

--- tests/test_osm.py
+++ tests/test_osm.py
@@ -1,16 +1,12 @@
-import os
-import osmnx as ox
-import geopandas as gpd
-import pandas as pd
-from datetime import datetime
-from retrying import retry
-
-# Update this line to use the new settings module
-ox.settings.timeout = 60
-
-from .src.data_extraction.osm import fetch_osm
-
 def test_osm():
-    iso_country_code = "PHL"
-    admin_level = 2
-    fetch_osm(iso_country_code, admin_level)
+    import os
+    import osmnx as ox
+    import geopandas as gpd
+    import pandas as pd
+    from datetime import datetime
+    from retrying import retry
+
+    # Update this line to use the new settings module
+    ox.settings.timeout = 60
+
+    from .src.data_extraction.osm import fetch_osm
