568b43579d3c5c56b892dc85321259f63515c3b3
Dominic Ligot <dominic.ligot@cirrolytix.com>
2023-09-11 21:52:01 +0800

    update all tests

--- tests/test_google_trends.py
+++ tests/test_google_trends.py
@@ -1,11 +1,10 @@
-from pytrends.request import TrendReq
-import pandas as pd
-import time
-import os
-import random
-import logging
-
-from .src.data_extraction.google_trends import fetch_google_trends
-
 def test_google_trends():
-    fetch_google_trends('PHL', '2016-01-10', '2021-01-10')
+    from pytrends.request import TrendReq
+    import pandas as pd
+    import time
+    import os
+    import random
+    import logging
+
+    from .src.data_extraction.google_trends import fetch_google_trends
+
--- tests/test_nasa_worldview.py
+++ tests/test_nasa_worldview.py
@@ -1,12 +1,10 @@
-import requests
-import wget
-import datetime
-import os
-import json
-import time
-from datetime import timedelta
-
-from .src.data_extraction.nasa_worldview import fetch_nasa_worldview
-
 def test_nasa_worldview():
-    fetch_nasa_worldview("PHL", "Zamboanga Sibugay", "Zamboanga Peninsula", "2016-01-10", "2021-01-10", "W")
+    import requests
+    import wget
+    import datetime
+    import os
+    import json
+    import time
+    from datetime import timedelta
+
+    from .src.data_extraction.nasa_worldview import fetch_nasa_worldview
