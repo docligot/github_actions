311241c926dc8c2191c6dd42d8a8142143995fb8
Dominic Ligot <dominic.ligot@cirrolytix.com>
2023-09-13 16:32:20 +0800

    redo osm

--- tests/test_osm.py
+++ tests/test_osm.py
@@ -8,9 +8,14 @@ from retrying import retry
 # Update this line to use the new settings module
 ox.settings.timeout = 60
 
-from .src.data_extraction.osm import retry_if_file_not_found_error
+from .src.data_extraction.osm import retry_if_file_not_found_error, create_directory, fetch_osm
 
 def test_retry_if_file_not_found_error():
     assert retry_if_file_not_found_error(1) == 1
 
+def test_create_directory():
+    assert create_directory(1) == 1
+
+def test_fetch_osm():
+    assert fetch_osm(1, 2) == "1 2"
 
